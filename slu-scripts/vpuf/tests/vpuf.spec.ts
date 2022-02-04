ffc84d79f6e21f5b24c70d52fe0a4b673dbb6fe5
Juri Bogatyrjow <juri.bogatyrjow@gmail.com>
2022-02-04 10:24:56 +0100

    adjusted vpuf.ts and tests

--- slu-scripts/vpuf/tests/vpuf.spec.ts
+++ slu-scripts/vpuf/tests/vpuf.spec.ts
@@ -6,23 +6,31 @@ describe('VPUF Tests', () => {
 	let encryptedData: string;
 	let decryptedData: string;
 	let data: string;
+	let storedEncryptedData: string;
 
 	beforeEach(() => {
 		key = vpuf.createKey('./vpuf/tests/data/unclonable.txt');
-		encryptedData = vpuf.encrypt('./vpuf/tests/data/data.json', key);
-		decryptedData = vpuf.decrypt(encryptedData, key);
-		data = fs.readFileSync('./vpuf/tests/data/data.json', 'utf-8');
+		encryptedData = vpuf.encrypt('./vpuf/tests/data/data.json', key, './vpuf/tests/data');
+		storedEncryptedData = fs.readFileSync('./vpuf/tests/data/data.json.enc', 'utf-8');
+		decryptedData = vpuf.decrypt(storedEncryptedData, key);
+		data = fs.readFileSync('./vpuf/tests/data/data.json', 'utf-8');	
 	});
 	test('should create the same key', () => {
 		const key2 = vpuf.createKey('./vpuf/tests/data/unclonable.txt');
 		expect(key).toBe(key2);
 	});
 	test('should encrypt data', () => {
+		expect(storedEncryptedData).toBe(encryptedData)
 		expect(encryptedData).not.toBe(data);
 		expect(encryptedData).toContain('U2FsdGVkX1');
+
 	});
 	test('should decrypt data', () => {
 		expect(decryptedData).toBe(data);
 		expect(decryptedData).not.toContain('U2FsdGVkX1');
 	});
+
+	afterAll(() => {
+		fs.rmSync('./vpuf/tests/data/data.json.enc')
+	})
 });
--- slu-scripts/vpuf/vpuf.ts
+++ slu-scripts/vpuf/vpuf.ts
@@ -6,9 +6,10 @@ const createKey = (filePath: string): string => {
 	const key = cryptoJs.SHA256(seed).toString();
 	return key;
 };
-const encrypt = (filePath: string, key: string): string => {
+const encrypt = (filePath: string, key: string, dest: string): string => {
 	const data = fs.readFileSync(filePath, 'utf-8');
 	var encryptedData = cryptoJs.AES.encrypt(data, key).toString();
+	fs.writeFileSync(dest + '/data.json.enc', encryptedData, 'utf-8')
 	return encryptedData;
 };
 const decrypt = (encrypted: string, key: string): string => {
