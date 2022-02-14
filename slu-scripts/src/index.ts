45ee3d945e154257731fc5f6038ff0c4644977de
Dominic Zettl <dominic.zettl@iota.org>
2022-02-14 16:40:25 +0100

    small adjustment

--- slu-scripts/src/index.ts
+++ slu-scripts/src/index.ts
@@ -3,15 +3,20 @@ import yargs from 'yargs';
 import { encryptData, decryptData, sendAuthProof } from './auth-proof/auth-proof';
 
 const argv = yargs
-	.command('encrypt', 'Encrypting data')
-	.option('key_file',{describe: 'The location of the key file.'})
-	.option('input',{describe: 'The location of the data to encrypt.'})
-	.option('dest', {describe: 'The destination where the encrypted data has to be stored.'})
-	.command('send-proof', 'Decrypt data and send auth proof of device')
-	.option('key_file',{describe: 'The location of the key file.'})
-	.option('interval', {describe: 'The interval in millisecond during requests to the collector microservice are done.'})
-	.option('input_enc', {describe: 'The location of the encrypted data.'})
-	.option('collector_url', {describe: 'The url of the microservice collector'})
+	.command('encrypt', 'Encrypting data', (yargs) =>
+		yargs
+			.option('key_file', { desc: 'The location of the key file.' })
+			.option('input', { desc: 'The location of the data to encrypt.' })
+			.option('dest', { desc: 'The destination where the encrypted data has to be stored.' })
+	)
+	.command('send-proof', 'Decrypt data and send auth proof of device', (yargs) =>
+		yargs
+			.option('key_file', { describe: 'The location of the key file.' })
+			.option('interval', { describe: 'The interval in millisecond during requests to the collector microservice are done.' })
+			.option('input_enc', { describe: 'The location of the encrypted data.' })
+			.option('collector_url', { describe: 'The url of the microservice collector' })
+	)
+
 	.help().argv;
 
 export async function sendingProof() {
@@ -23,7 +28,7 @@ export async function sendingProof() {
 			encryptData(keyFilePath, inputData, destination);
 		} catch (ex: any) {
 			console.error(ex.message);
-			process.exit(1)
+			process.exit(1);
 		}
 	} else if (argv._.includes('send-proof')) {
 		const interval: string | undefined = process.env.npm_config_interval;
@@ -39,7 +44,7 @@ export async function sendingProof() {
 			}
 		} catch (ex: any) {
 			console.error(ex.message);
-			process.exit(1)
+			process.exit(1);
 		}
 	} else {
 		yargs.showHelp('log');
