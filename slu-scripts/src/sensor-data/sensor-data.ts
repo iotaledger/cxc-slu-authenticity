c78041577ccaa95b6b812119c806a62a94da4b6d
Juri Bogatyrjow <juri.bogatyrjow@gmail.com>
2022-02-21 18:13:05 +0100

    implemented review feedback

--- slu-scripts/src/sensor-data/sensor-data.ts
+++ slu-scripts/src/sensor-data/sensor-data.ts
@@ -5,23 +5,21 @@ import { createKey, decrypt } from '../vpuf/vpuf';
 export async function sendData(
 	encryptedDataPath: string | undefined,
 	keyFilePath: string | undefined,
-	isConfiguration: string | undefined
+	isConfigPath: string | undefined,
+	payloadData: any
 ): Promise<ChannelData> {
-	if (isConfiguration && encryptedDataPath && keyFilePath) {
+	if (isConfigPath && encryptedDataPath && keyFilePath) {
 		const encryptedData = fs.readFileSync(encryptedDataPath, 'utf-8');
 		const key = createKey(keyFilePath);
 		const decryptedData = decrypt(encryptedData, key);
-		const data = JSON.parse(decryptedData);
-		const identity = data.identity;
-		const channelAddress = data.channelAddress;
-		let isConfig = fs.readFileSync(isConfiguration, 'utf-8');
-		isConfig = Buffer.from(isConfig).toString('utf-8');
+		const { identity, channelAddress } = JSON.parse(decryptedData);
+		let isConfig = fs.readFileSync(isConfigPath, 'utf-8');
 		const clientConfig: ClientConfig = JSON.parse(isConfig);
 		try {
 			const client = new ChannelClient(clientConfig);
 			await client.authenticate(identity.doc.id, identity.key.secret);
 			const response = await client.write(channelAddress, {
-				payload: { temperature: '60 degrees' }
+				payload: payloadData
 			});
 			return response;
 		} catch (ex: any) {
--- slu-scripts/src/sensor-data/tests/sensor-data.spec.ts
+++ slu-scripts/src/sensor-data/tests/sensor-data.spec.ts
@@ -7,29 +7,30 @@ const keyFilePath: string | undefined = process.env.npm_config_key_file;
 const inputData: string | undefined = process.env.npm_config_input;
 const destination: string | undefined = process.env.npm_config_dest;
 const encryptedDataPath: string | undefined = process.env.npm_config_input_enc;
-const isConfiguration: string | undefined = process.env.npm_config_config;
+const isConfigPath: string | undefined = process.env.npm_config_is_config_file;
 
 describe('Send sensor data tests', () => {
 	beforeAll(() => {
 		encryptData(keyFilePath, inputData, destination);
 	});
 
-	it('should fail to send data: connection error', async () => {
+	it('should fail to send data: no valid api key provided', async () => {
 		try {
-			await sendData(encryptedDataPath, keyFilePath, isConfiguration);
+			await sendData(encryptedDataPath, keyFilePath, isConfigPath, {});
 		} catch (ex: any) {
-			expect(ex.code).toBe('ECONNREFUSED');
+			expect(ex.response.status).toBe(401);
 		}
 	});
 	it('should fail to send data: env variable not provided', async () => {
 		try {
-			await sendData(encryptedDataPath, keyFilePath, '');
+			await sendData(encryptedDataPath, keyFilePath, '', { temperature: '60 degrees' });
 		} catch (ex: any) {
 			expect(ex.message).toBe('One or all of the env variables are not provided: --input_enc, --key_file, --config');
 		}
 	});
 
 	it('should send data', async () => {
+		const payloadData = { temperature: '60 degrees' };
 		const dataString = fs.readFileSync(inputData!, 'utf-8');
 		const data = JSON.parse(dataString);
 		const channelData: ChannelData = {
@@ -42,9 +43,9 @@ describe('Send sensor data tests', () => {
 		};
 		const autheticateSpy = jest.spyOn(ChannelClient.prototype, 'authenticate').mockResolvedValue();
 		const writeSpy = jest.spyOn(ChannelClient.prototype, 'write').mockResolvedValue(channelData);
-		const response = await sendData(encryptedDataPath, keyFilePath, isConfiguration);
+		const response = await sendData(encryptedDataPath, keyFilePath, isConfigPath, payloadData);
 		expect(autheticateSpy).toHaveBeenCalledWith(data.identity.doc.id, data.identity.key.secret);
-		expect(writeSpy).toHaveBeenCalledWith(data.channelAddress, { payload: { temperature: '60 degrees' } });
+		expect(writeSpy).toHaveBeenCalledWith(data.channelAddress, { payload: payloadData });
 		expect(response).toBe(channelData);
 	});
 
