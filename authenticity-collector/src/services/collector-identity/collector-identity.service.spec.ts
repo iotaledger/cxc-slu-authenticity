82a8588b9959e59f1cb18c15f1d356caa313f39c
Juri Bogatyrjow <juri.bogatyrjow@gmail.com>
2022-04-06 13:46:22 +0200

    implemented review feedback

--- authenticity-collector/src/services/collector-identity/collector-identity.service.spec.ts
+++ authenticity-collector/src/services/collector-identity/collector-identity.service.spec.ts
@@ -1,6 +1,6 @@
 import { ConfigService } from '@nestjs/config';
 import { Test, TestingModule } from '@nestjs/testing';
-import { IdentityClient, IdentityJson } from 'iota-is-sdk/lib';
+import { IdentityClient } from 'iota-is-sdk/lib';
 import { defaultConfig } from '../../configuration';
 import { CollectorIdentityService } from './collector-identity.service';
 import * as fs from 'fs';
@@ -28,92 +28,13 @@ describe('CollectorIdentityService', () => {
 		expect(identityClientSpy).toBeCalled();
 	});
 
-	it('provided with the right id and secret', async () => {
-		const identityClientSpy = jest.spyOn(IdentityClient.prototype, 'authenticate').mockResolvedValue();
-
-		await service.checkCollectorIdentity();
-
-		expect(identityClientSpy).toBeCalled();
-	});
-
-	it('provided env vars are matching with identitiy file', async () => {
-		const identityJson: IdentityJson = {
-			doc: {
-				id: 'iota:did:12345'
-			},
-			key: {
-				secret: 'secret'
-			}
-		} as IdentityJson;
-
-		fs.writeFileSync('./collector-identity.json', JSON.stringify(identityJson));
-		const identityClientSpy = jest.spyOn(IdentityClient.prototype, 'authenticate').mockResolvedValue();
-
-		await service.checkCollectorIdentity();
-
-		expect(identityClientSpy).toBeCalled();
-	});
-
-	it('provided COLLECTOR_DID are not matching the id of identity file', async () => {
-		process.env.COLLECTOR_DID = 'did:iota:wrong';
-		const identityJson: IdentityJson = {
-			doc: {
-				id: 'did:iota54321'
-			},
-			key: {
-				secret: 'secret'
-			}
-		} as IdentityJson;
-
-		fs.writeFileSync('./collector-identity.json', JSON.stringify(identityJson));
-
-		await service.checkCollectorIdentity();
-
-		expect(process.env.COLLECTOR_DID).toBe(identityJson.doc.id);
-	});
-
-	it('provided COLLECTOR_SECRET are not matching the secret of identity file', async () => {
-		process.env.COLLECTOR_SECRET = 'wrongSecret';
-		const identityJson: IdentityJson = {
-			doc: {
-				id: 'iota:did:12345'
-			},
-			key: {
-				secret: 'secret'
-			}
-		} as IdentityJson;
-
-		fs.writeFileSync('./collector-identity.json', JSON.stringify(identityJson));
-
-		await service.checkCollectorIdentity();
-
-		expect(process.env.COLLECTOR_SECRET).toBe(identityJson.key.secret);
-	});
-
-	it('should create new identity: no env vars provided', async () => {
+	it('should throw error: no env vars provided', async () => {
 		process.env.COLLECTOR_DID = '';
 		process.env.COLLECTOR_SECRET = '';
-		const identityJson: IdentityJson = {
-			doc: {
-				id: 'did:iota54321'
-			},
-			key: {
-				secret: 'otherSecret'
-			}
-		} as IdentityJson;
 
-		const identitiyClientSpy = jest.spyOn(IdentityClient.prototype, 'create').mockResolvedValue(identityJson);
-		const writeFileSpy = jest.spyOn(fs, 'writeFileSync');
+		const response = service.checkCollectorIdentity();
 
-		await service.checkCollectorIdentity();
+		expect(response).rejects.toThrowError('no COLLECTOR_DID and/or COLLECTOR_SECRET is provided')
 
-		expect(process.env.COLLECTOR_DID).toBe(identityJson.doc.id);
-		expect(process.env.COLLECTOR_SECRET).toBe(identityJson.key.secret);
-		expect(identitiyClientSpy).toBeCalled();
-		expect(writeFileSpy).toBeCalled();
-	});
-
-	afterAll(() => {
-		fs.rmSync('collector-identity.json');
 	})
 });
--- authenticity-collector/src/services/collector-identity/collector-identity.service.ts
+++ authenticity-collector/src/services/collector-identity/collector-identity.service.ts
@@ -5,30 +5,15 @@ import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class CollectorIdentityService {
-	constructor(private configService: ConfigService, @Inject('IdentityClient') private identityClient: IdentityClient) {}
+	constructor(private configService: ConfigService, @Inject('IdentityClient') private identityClient: IdentityClient) { }
 
 	async checkCollectorIdentity() {
 		const id: string = this.configService.get('COLLECTOR_DID');
 		const secret: string = this.configService.get('COLLECTOR_SECRET');
-		let collectorIdentity: IdentityJson;
 		if (id && secret) {
-			try {
-				collectorIdentity = JSON.parse(fs.readFileSync('./collector-identity.json', 'utf-8'));
-				if (collectorIdentity && (collectorIdentity?.doc?.id !== id || collectorIdentity?.key?.secret !== secret)) {
-					//if provided env vars are not the same like in the file, assign them with the values of the file
-					process.env.COLLECTOR_DID = collectorIdentity.doc.id;
-					process.env.COLLECTOR_SECRET = collectorIdentity.key.secret;
-					fs.rmSync('./collector-identity.json');
-				}
-			} catch (ex: any) {
-				//No identity file available, just authenticate the identity with the provided env vars
-				await this.identityClient.authenticate(id, secret);
-			}
+			await this.identityClient.authenticate(id, secret);
 		} else {
-			collectorIdentity = await this.identityClient.create('authenticity-collector ' + Math.ceil(Math.random() * 1000));
-			process.env.COLLECTOR_DID = collectorIdentity.doc.id;
-			process.env.COLLECTOR_SECRET = collectorIdentity.key.secret;
-			fs.writeFileSync('collector-identity.json', collectorIdentity.toString());
+			throw new Error('no COLLECTOR_DID and/or COLLECTOR_SECRET is provided')
 		}
 	}
 }
