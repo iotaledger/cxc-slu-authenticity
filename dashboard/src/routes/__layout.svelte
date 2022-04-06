79087d19f08cc0bba72bd8402c5407c4fa1e8932
evavirseda <evirseda@boxfish.studio>
2022-04-06 15:20:56 +0200

    Add dashboard route

--- dashboard/src/routes/__layout.svelte
+++ dashboard/src/routes/__layout.svelte
@@ -1,19 +1,20 @@
-
 <script>
-	import Header from "$lib/header/Header.svelte";
-  import { Container } from 'sveltestrap';
-  import { NotificationManager } from '@iota/is-ui-components';
+	import Header from '$lib/header/Header.svelte';
+	import { Container } from 'sveltestrap';
+	import { NotificationManager } from '@iota/is-ui-components';
 </script>
 
 <svelte:head>
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css">
+	<link
+		rel="stylesheet"
+		href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css"
+	/>
 </svelte:head>
 
 <Header />
 
-<NotificationManager></NotificationManager>
+<NotificationManager />
 
 <Container>
-  <slot></slot>
+	<slot />
 </Container>
-
---
+++ dashboard/src/routes/dashboard/index.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import { authenticatedUserDID, isAuthenticated } from '@iota/is-ui-components';
+	import { showNotification, NotificationType } from '@iota/is-ui-components';
+	import { Button } from 'sveltestrap';
+
+	function push() {
+		showNotification({
+			type: NotificationType.Info,
+			message: 'Example of info from Dashboard'
+		});
+	}
+</script>
+
+<h3>Dashboard</h3>
+
+User DID: {$authenticatedUserDID}
+
+<br />
+
+Is Authenticated: {$isAuthenticated}
+
+<br />
+
+<Button on:click={push}>Notification Example</Button>
