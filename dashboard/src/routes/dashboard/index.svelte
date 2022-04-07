6500d038929681465fa7e02fa06aa538d4b6adc8
Ruben Alvarez <ralvarez@boxfish.studio>
2022-04-07 15:30:24 +0200

    Cleanup

--- dashboard/src/routes/dashboard/index.svelte
+++ dashboard/src/routes/dashboard/index.svelte
@@ -1,39 +1,23 @@
 <script lang="ts">
-	import { isAuthenticated } from '@iota/is-ui-components';
-	import { goto } from '$app/navigation';
-	import { browser } from '$app/env';
-	import type { ActionButton, TableData } from '@iota/is-ui-components';
-	import {
-		Icon,
-		isAsyncLoadingIdentities,
-		ListManager,
-		NotificationManager,
-		searchIdentitiesResults,
-		selectedIdentity,
-		stopReadingChannel
-	} from '@iota/is-ui-components';
-	import { Container, Row } from 'sveltestrap';
 	import DeviceDetails from '$lib/device-details/device-details.svelte';
+	import type { Device } from '$lib/device/types';
+	import type { ActionButton, TableData } from '@iota/is-ui-components';
+	import { Icon, ListManager, NotificationManager } from '@iota/is-ui-components';
+	import { Container, Row } from 'sveltestrap';
 
-	$: {
-		if (browser && !$isAuthenticated) {
-			goto('/');
-		}
+	enum State {
+		ListDevices = 'listDevices',
+		DeviceDetails = 'deviceDetails'
 	}
 
-	const IDENTITY_LIST_BUTTONS: ActionButton[] = [
+	const DEVICE_LIST_BUTTONS: ActionButton[] = [
 		{
-			label: 'Add device',
-			onClick: addNewDevice,
+			label: 'Create device',
+			onClick: createDevice,
 			icon: 'plus',
 			color: 'dark'
 		}
 	];
-
-	enum State {
-		ListDevices = 'listDevices',
-		DeviceDetail = 'deviceDetail'
-	}
 
 	let devices = [
 		{
@@ -44,47 +28,47 @@
 			messages: 'messages'
 		}
 	];
-	let state: State = State.ListDevices;
-	let loading: boolean = false;
-	let query: string = '';
-	let message: string;
 
-	$: $selectedIdentity, updateState();
-	$: state;
-	$: message =
-		$isAsyncLoadingIdentities || loading || $searchIdentitiesResults?.length
-			? null
-			: 'No devices found';
+	let state: State = State.ListDevices;
+	let message: string;
+	let selectedDevice: Device;
+
+	$: selectedDevice, updateState();
+	$: message = devices?.length ? 'No devices found' : undefined;
 
 	$: tableData = {
-		headings: ['Device'],
+		headings: ['Device Id', 'Nonce'],
 		rows: devices.map((device) => ({
-			onClick: () => handleSelectIdentity(device),
+			onClick: () => handleSelectDevice(device),
 			content: [
 				{
 					icon: 'nut',
 					boxColor: 'green',
-					value: device.id
+					value: device.id ?? '-'
+				},
+				{
+					value: device.nonce ?? '-'
 				}
 			]
 		}))
 	} as TableData;
 
 	async function updateState(): Promise<void> {
-		if ($selectedIdentity) {
-			state = State.DeviceDetail;
+		if (selectedDevice) {
+			state = State.DeviceDetails;
 		} else {
 			state = State.ListDevices;
-			stopReadingChannel();
+			// stopReadingChannel();
 		}
 	}
 
-	function handleSelectIdentity(device): void {
-		selectedIdentity.set(device);
+	function handleSelectDevice(device): void {
+		selectedDevice = device;
 	}
 
-	function addNewDevice() {
-		let newDevices = [
+	function createDevice() {
+		// TODO: Add correct logic here
+		devices = [
 			...devices,
 			{
 				id: `device ${++devices.length}`,
@@ -94,11 +78,10 @@
 				messages: 'messages'
 			}
 		];
-		devices = newDevices;
 	}
 
 	function handleBackClick(): void {
-		selectedIdentity.set(null);
+		selectedDevice = null;
 	}
 </script>
 
@@ -107,24 +90,17 @@
 		<h1>Device Manager</h1>
 	</Row>
 	{#if state === State.ListDevices}
-		<ListManager
-			showSearch
-			{tableData}
-			{message}
-			loading={loading || $isAsyncLoadingIdentities}
-			actionButtons={IDENTITY_LIST_BUTTONS}
-			bind:searchQuery={query}
-		/>
-	{:else if state === State.DeviceDetail}
+		<ListManager {tableData} {message} actionButtons={DEVICE_LIST_BUTTONS} />
+	{:else if state === State.DeviceDetails}
 		<div class="mb-4 align-self-start">
 			<button on:click={handleBackClick} class="btn d-flex align-items-center">
 				<Icon type="arrow-left" size={16} />
 				<span class="ms-2">Back</span>
 			</button>
 		</div>
-		{#each devices as device}
-			<DeviceDetails {device} />
-		{/each}
+		{#if selectedDevice}
+			<DeviceDetails device={selectedDevice} />
+		{/if}
 	{/if}
 </Container>
 
