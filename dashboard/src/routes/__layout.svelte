863506a683ad92216366076dea30a5d3c9ce0a8b
Ruben Alvarez <ralvarez@boxfish.studio>
2022-04-12 11:42:26 +0200

    Fix: Remove duplicate sass dependency and remove Bootstrap import from CDN

--- dashboard/src/routes/__layout.svelte
+++ dashboard/src/routes/__layout.svelte
@@ -13,13 +13,6 @@
 	}
 </script>
 
-<svelte:head>
-	<link
-		rel="stylesheet"
-		href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css"
-	/>
-</svelte:head>
-
 <Header />
 <NotificationManager />
 <main>
