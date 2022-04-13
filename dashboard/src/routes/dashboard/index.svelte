73d3d336e0b5fd5f46e6aefb1712594bd4e6e4ba
Ruben Alvarez <ralvarez@boxfish.studio>
2022-04-13 17:35:58 +0200

    Cleanup index

--- dashboard/src/routes/dashboard/index.svelte
+++ dashboard/src/routes/dashboard/index.svelte
@@ -1,90 +1,43 @@
 <script lang="ts">
 	import { createDevice, getDevices } from '$lib';
+	import { deviceCreationProgress } from '$lib/store';
 	import type { Device } from '$lib/types';
 	import {
 		authenticationData,
 		Icon,
 		ListManager,
-		NotificationManager,
 		type ActionButton,
 		type TableData
 	} from '@iota/is-ui-components';
 	import { onMount } from 'svelte';
 	import { Container, Row } from 'sveltestrap';
 	import { DeviceDetails, ProgressBar } from '../../components';
-	import { deviceCreationProgress } from '$lib/store';
 
 	enum State {
 		ListDevices = 'listDevices',
 		DeviceDetails = 'deviceDetails'
 	}
 
-	let devices = [];
 	let loading: boolean = false;
 	let state: State = State.ListDevices;
 	let message: string;
 	let selectedDevice: Device;
 	let query: string;
 	let searchResults: Device[] = [];
-	let CREATE_DEVICE_BUTTON: ActionButton = {
-		label: 'Create device',
+	let devices: Device[] = [];
+	let createDeviceButton: ActionButton;
+
+	$: createDeviceButton = {
 		onClick: handleCreateDevice,
 		icon: 'plus',
-		color: 'dark'
+		color: 'dark',
+		label: loading ? 'Creating device...' : 'Create device',
+		loading,
+		disabled: loading
 	};
-
-	onMount(async () => {
-		await loadDevices();
-	});
-
-	async function loadDevices() {
-		devices = await getDevices($authenticationData?.did);
-		searchResults = devices;
-	}
-
-	function updateLoading(): void {
-		CREATE_DEVICE_BUTTON = {
-			icon: loading ? undefined : 'plus',
-			onClick: handleCreateDevice,
-			label: loading ? 'Creating device...' : 'Create device',
-			loading,
-			disabled: loading,
-			color: 'dark'
-		};
-	}
-
-	async function updateState(): Promise<void> {
-		if (selectedDevice) {
-			state = State.DeviceDetails;
-		} else {
-			state = State.ListDevices;
-		}
-	}
-
-	function handleSelectDevice(device: Device): void {
-		selectedDevice = device;
-	}
-
-	async function handleCreateDevice(): Promise<void> {
-		loading = true;
-		await createDevice();
-		loadDevices();
-		loading = false;
-	}
-
-	function handleBackClick(): void {
-		selectedDevice = null;
-	}
-
-	function onSearch() {
-		searchResults = devices.filter((d) => d.id?.includes(query));
-	}
-
 	$: selectedDevice, updateState();
 	$: message = 'No devices found';
-	$: loading, updateLoading();
 	$: query, onSearch();
-
 	$: tableData = {
 		headings: ['Device Id', 'Related channel'],
 		rows: searchResults.map((device) => ({
@@ -101,6 +54,42 @@
 			]
 		}))
 	} as TableData;
+
+	onMount(async () => {
+		await loadDevices();
+	});
+
+	async function loadDevices() {
+		devices = await getDevices($authenticationData?.did);
+		searchResults = devices;
+	}
+
+	function updateState(): void {
+		if (selectedDevice) {
+			state = State.DeviceDetails;
+		} else {
+			state = State.ListDevices;
+		}
+	}
+
+	function handleSelectDevice(device: Device): void {
+		selectedDevice = device;
+	}
+
+	async function handleCreateDevice(): Promise<void> {
+		loading = true;
+		await createDevice();
+		await loadDevices();
+		loading = false;
+	}
+
+	function handleBackClick(): void {
+		selectedDevice = null;
+	}
+
+	function onSearch() {
+		searchResults = devices.filter((d) => d.id?.includes(query));
+	}
 </script>
 
 <Container class="relative py-5">
@@ -113,7 +102,7 @@
 			title="My devices"
 			{tableData}
 			{message}
-			actionButtons={[CREATE_DEVICE_BUTTON]}
+			actionButtons={[createDeviceButton]}
 			bind:searchQuery={query}
 		/>
 		{#if loading}
@@ -133,8 +122,6 @@
 		{/if}
 	{/if}
 </Container>
-
-<NotificationManager />
 
 <style lang="scss">
 	:global(.btn:hover) {
