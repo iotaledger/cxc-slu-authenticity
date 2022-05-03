7e3e78102ced367bcefbea24016177e805e8756a
jacekroszkowiak <jacek.roszkowiak@iota.org>
2022-05-03 19:08:24 +0200

    swagger docs finished

--- one-shot-device/src/main.ts
+++ one-shot-device/src/main.ts
@@ -1,18 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { SwaggerModule, DocumentBuilder, SwaggerDocumentOptions } from '@nestjs/swagger';
 import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 
+	const options: SwaggerDocumentOptions = {
+		operationIdFactory: (controllerKey: string, methodKey: string) => methodKey
+	};
+
 	const config = new DocumentBuilder()
 		.setTitle('One-Shot-Device-Api-Reference')
-		.setDescription('One-Shot-Device documentation')
+		.setDescription(
+			'One-Shot-Device is a microservice that allows the creator to register and authenticate devices for the CityXChange. It uses Integration Services and IOTA SSI.'
+		)
 		.setVersion('1.0')
 		.addTag('one-shot-device')
 		.build();
-	const document = SwaggerModule.createDocument(app, config);
+
+	const document = SwaggerModule.createDocument(app, config, options);
 	SwaggerModule.setup('one-shot-device-docs', app, document);
 
 	await app.listen(process.env.PORT || 3000);
