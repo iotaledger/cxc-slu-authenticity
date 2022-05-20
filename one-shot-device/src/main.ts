1708102b2390bc6e79c83ec91df9f5172daaeeb3
jacekroszkowiak <jacek.roszkowiak@iota.org>
2022-05-20 12:28:26 +0200

    1st commit

--- one-shot-device/src/main.ts
+++ one-shot-device/src/main.ts
@@ -5,6 +5,7 @@ import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
+	app.enableCors();
 
 	const options: SwaggerDocumentOptions = {
 		operationIdFactory: (controllerKey: string, methodKey: string) => methodKey
