372abd13e7b814fbc19cb82922c45643b4ec91ab
Juri Bogatyrjow <juri.bogatyrjow@gmail.com>
2022-05-23 13:54:42 +0200

    boolean var to open dialog after closing is now bind in dashboard

--- dashboard/src/components/device-name.svelte
+++ dashboard/src/components/device-name.svelte
@@ -1,20 +1,28 @@
 <script lang="ts">
-	import { Button,Input, Modal, ModalBody, ModalHeader } from 'sveltestrap';
-    export let value = "";
-    export let onClick = () => {};
-    export let isOpen = false;
-    const toggle = () => (isOpen = !isOpen);
+	import { Button, Input, Modal, ModalBody, ModalHeader } from 'sveltestrap';
+	export let value = '';
+	export let onClick = () => {};
+	export let isOpen = false;
+	const toggle = () => {
+		isOpen = !isOpen;
+		value = '';
+	};
 </script>
 
-
 <div class="w-100">
-    <Modal {isOpen} backdrop={false} {toggle}>
-        <ModalHeader class="px-4 pt-3" {toggle}>Device Name</ModalHeader>
-        <ModalBody class="px-4 pb-4">
-            <Input bsSize="lg" bind:value placeholder="Device Name"/>
-            <div class="modal-footer">
-                <Button disabled={value.length === 0} size="lg" color="primary" type="submit" on:click={onClick}>OK</Button>
-            </div>
-        </ModalBody>
-    </Modal>
+	<Modal {isOpen} {toggle}>
+		<ModalHeader class="px-4 pt-3" {toggle}>Device Name</ModalHeader>
+		<ModalBody class="px-4 pb-4">
+			<Input bsSize="lg" bind:value placeholder="Device Name" />
+			<div class="modal-footer">
+				<Button
+					disabled={value.length === 0}
+					size="lg"
+					color="primary"
+					type="submit"
+					on:click={onClick}>OK</Button
+				>
+			</div>
+		</ModalBody>
+	</Modal>
 </div>
